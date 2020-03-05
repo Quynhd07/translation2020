8461676808133274f25d8e6ffd576eb6b14b813d
Quynh <qtd88244@gmail.com>
2020-03-05 10:40:43 -0600

    fix git history

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect, request, flash
+
+from translate import Translator 
+
+# from jinja2 import StrictUndefined
+
+# from requests import get 
+
+import os 
+
+import sources
+
+app = Flask(__name__)
+# app.secret_key = os.environ['translation2020_key']
+app.secret_key = 'test'
+
+# API_KEY = os.environ[]
+
+@app.route('/')
+def homepage():
+    """display headlines"""
+
+    vietnamese = Translator(to_lang="vi")
+
+    outlets = [sources.get_apnews, sources.get_npr]
+    names = ['APNEWS', 'NPR']
+
+    vi_translation = []
+    
+    for i in range(0, len(outlets)):
+        vi_translation.append(names[i])
+        for heading in outlets[i]():
+            translate_heading = vietnamese.translate(heading)
+            vi_translation.append(translate_heading)
+
+    return render_template("base.html", articles = vi_translation)
+
+
+
+if __name__ == "__main__":
+    # connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
---
+++ sources.py
@@ -0,0 +1,43 @@
+
+from requests import get
+from bs4 import BeautifulSoup
+
+
+
+def get_apnews():
+    url = "https://apnews.com/apf-politics"
+    # request the server content of apnews by using get()
+    # store the page's response in ap_news variable response 
+    response = get(url)
+
+    # create beautifulsoup object
+    # ap_news is now a response obj -> access its .text attribute 
+    # "html.parser" indicates that we want to parse using Python's built-in html parser
+    response_html = BeautifulSoup(response.text, "html.parser")
+    article_containers = response_html.find_all("h1", class_ ='Component-h1-0-2-63')
+    #return first 3 articles
+    headings = []
+    for i in range(0,3):
+        headings.append(article_containers[i].text)
+
+    return headings
+
+
+def get_npr():
+    url = "https://www.npr.org/sections/politics/"
+    # request the server content of apnews by using get()
+    # store the page's response in ap_news variable response 
+    response = get(url)
+
+    # create beautifulsoup object
+    # ap_news is now a response obj -> access its .text attribute 
+    # "html.parser" indicates that we want to parse using Python's built-in html parser
+    response_html = BeautifulSoup(response.text, "html.parser")
+    article_containers = response_html.find_all("div", class_ ='item-info-wrap')
+    #return first 3 articles
+    headings = []
+    for i in range(0,3):
+        headings.append(article_containers[i].text)
+
+    return headings
+
