dfe8d825e10eec33d4e2ac0327c8af4a2ac6cfc2
Quynh <qtd88244@gmail.com>
2020-03-11 17:05:26 -0500

    get most frequent tokens

--- tweets.py
+++ tweets.py
@@ -1,6 +1,10 @@
 import os 
 from tweepy import OAuthHandler, API, Cursor
-from collections import Counter 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from string import punctuation
+from re import sub
 
 
 consumer_key = os.getenv('translation_consumer_key')
@@ -11,7 +15,7 @@ access_token_secret = os.getenv('translation_access_token_secret')
 # use OAuthHandler class to handle authentication (no callback url needed)
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-# call API class to access entire twitter restful api methods 
+# API takes in authenication handler to be used; allows access to entire twitter restful api methods 
 api = API(auth)
 
 # dictionary containing last 5 tweets of each candidate
@@ -25,7 +29,7 @@ accounts = {
 }
 
 
-def get_tweets(accounts_dict) -> dict:
+def get_tweets(accounts) -> list:
     """From list of accounts, get their 5 most recent tweets."""
     # iterate through list of accounts
     for account in accounts:
@@ -33,15 +37,51 @@ def get_tweets(accounts_dict) -> dict:
         for status in Cursor(api.user_timeline,id=account, tweet_mode="extended").items(5):
             # check is status is a retweet, otherwise full text will be truncated
             if hasattr(status, 'retweeted_status'):
-                # add to list of tweets by account
-                accounts[account].append(status.retweeted_status.full_text)
-            # else, add tweet to list 
-            else:
-                accounts[account].append(status.full_text)
-    return accounts
+                status = status.retweeted_status
+            # get full text of each status
+            status = status.full_text
+            # remove http link at the end
+            status = sub(r' https://t.co/\S+','', status)
+            # add to list of tweets by account
+            accounts[account].append(status)
+
+    return tokenize_tweets(accounts)
 
 
-# tokenize tweets using NLTK or Counter
+# tokenize tweets using NLTK 
+def tokenize_tweets(accounts) -> list:
+    """returns list of tokenized tweets of all users without stopword and punctuation tokens"""
+    # create list to add tweets together 
+    all_tweets = []
+    # iterate through each account
+    for account in accounts:
+        # get list of tweets from each account
+        tweets = accounts[account]
+        # iterate through tweets for each account
+        for tweet in tweets:
+            all_tweets.extend(word_tokenize(tweet))
+
+    return remove_stop_words(all_tweets)
+
+
+def remove_stop_words(words_list) -> list:
+    """removes all stopsword and punctuation tokens from list of words"""
+    # create list of english stop words
+    stop_words = set(stopwords.words('english'))
+    # only add if it is not a stopword, punctuation, and longer than 3 chars 
+    nonstop_words = [word for word in words_list if word not in stop_words and word not in punctuation and len(word) > 3]
+    return nonstop_words
+    
+
+def get_frequent_words(words_list) -> list:
+    """returns list of most used tokens"""
+    return_list = []
+    # create an object of FreqDist and return top 5 words
+    frequency_count = FreqDist(words_list).most_common(5)
+    for word, frequency in frequency_count:
+        return_list.append(word)
+
+    return return_list 
 
 
 
